58eb08e8952747ed1fb77bc8d8713c16e61a4438
venkatreddy0765 <venkatreddy0765@gmail.com>
2017-11-08 15:31:27 -0500

    1

---
+++ maven/test/src/main/java/orgname/App.java
@@ -0,0 +1,13 @@
+package orgname;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
